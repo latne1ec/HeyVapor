725d9abb4003b358ddb1c80c48740a77d0ec999c
latne1ec <latne1ec@cmich.edu>
2016-10-05 15:07:51 -0700

    first commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "HeyVapor",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+    ]
+)
+
